36adbd06654ab2399e2e57fac208fcd201cbc69e
Anand Graves <anand@voorhoede.nl>
2019-06-11 13:29:26 +0200

    fix: editorconfig and spaces

--- arrays/array-delete-first/array-delete-first.js
+++ arrays/array-delete-first/array-delete-first.js
@@ -1,8 +1,8 @@
 function arrayDeleteFirst(value) {
-	if (!Array.isArray(value)) {
-		throw new TypeError('arrayDeleteFirst(): argument is not an Array.')
-	}
-	return value.slice(1)
+  if (!Array.isArray(value)) {
+    throw new TypeError('arrayDeleteFirst(): argument is not an Array.')
+  }
+  return value.slice(1)
 }
 
 export default arrayDeleteFirst
